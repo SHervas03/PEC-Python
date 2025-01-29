60035b243dc105979a4f1a5ac33250c9d3714db6
SHervas03 <sergiohervas9rgmail.com>
2025-01-29 11:56:47 +0100

    staging and masters

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import openpyxl
 import pandas as pd
+import xlwt
 
 def aaa(df_default):
     df_default['Descripción'] = df_default['Descripción'].fillna('No description')
@@ -15,19 +17,34 @@ def bbb(df, df_new_path):
 def ccc(df):
     print(df.info())
 
+def ddd(new_file_path, xls_file):
+    wb_xlsx = openpyxl.load_workbook(new_file_path)
+    sheet_xlsx = wb_xlsx.active
+    wb_xls = xlwt.Workbook()
+    sheet_xls = wb_xls.add_sheet('Hoja 1')
+    for row_idx, row in enumerate(sheet_xlsx.iter_rows(), start=0):
+        for col_idx, cell in enumerate(row, start=0):
+            sheet_xls.write(row_idx, col_idx, cell.value)
+
+    wb_xls.save(xls_file)
+    print("Sva")
 
 
 if __name__ == "__main__":
     df_default = "./Data/Dataset.xls"
     df_new =  './Data/New_Dataset.xlsx'
+    xls_file = './Data/New_Dataset.xls'
     df_dflt = pd.read_excel(df_default)
 
-    ccc(df_dflt)
-    df = aaa(df_dflt)
-    ccc(df)
-    bbb(df, df_new)
-    df_nw = pd.read_excel(df_new)
-    ccc(df_nw)
+    # ccc(df_dflt)
+    # df = aaa(df_dflt)
+    # ccc(df)
+    # bbb(df, df_new)
+    # df_nw = pd.read_excel(df_new)
+    # ccc(df_nw)
+    ddd(df_new, xls_file)
+    df = pd.read_excel(xls_file)
+    print(df.info())
 
 
 
